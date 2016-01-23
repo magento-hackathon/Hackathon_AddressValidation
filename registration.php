6b356c90da256a6036beb0aa345e05aeb858ab4b
Arjen Miedema <arjen.miedema@mediact.nl>
2016-01-23 10:55:07 +0100

    Added registration file

---
+++ registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ *
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Hackathon_AddressValidation',
+    __DIR__
+);
